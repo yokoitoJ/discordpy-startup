7b732ee5df020f3acab3608eb6f254e890ed29a6
yokoitoJ <58509524+yokoitoJ@users.noreply.github.com>
2019-12-05 01:08:20 +0900

    dada

--- discordbot.py
+++ discordbot.py
@@ -1,25 +1,21 @@
-import discord  
-import os  
+from discord.ext import commands
+import os
+import traceback
 
-client = discord.Client()  
-TOKEN = os.environ['DISCORD_BOT_TOKEN']  
-ID_CHANNEL_QUESTION = # 質問チャンネルID  
-ID_CATEGORY_QA = # 質問スレカテゴリID  
-
-
-@client.event  
-async def on_message(message):  
-    if message.channel.id == ID_CHANNEL_QUESTION:  
-        await qa_thread(message)  
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
 
-async def qa_thread(message):  
-    category_qa = client.get_channel(ID_CATEGORY_QA)  
-    channel_name = f'q{len(category_qa.text_channels)}'  
-    payload = {'name': channel_name, 'category': category_qa, 'position': 0}  
-    channel_qa = await message.guild.create_text_channel(**payload)  
-    await channel_qa.send(message.jump_url)  
-    await client.get_channel(ID_CHANNEL_QUESTION).edit(position=0)  
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
 
-client.run(TOKEN)  
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+bot.run(token)
